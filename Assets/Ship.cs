82e1b84fc955b858f3c10b502c26998827dfd3f6
Bradley Crusco <bcrusco627@gmail.com>
2015-01-15 15:39:31 -0500

    Cube can move across the screen.

---
+++ Assets/Ship.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 updatedPosition = gameObject.transform.position;
+		updatedPosition.x += .001f;
+		gameObject.transform.position = updatedPosition;
+	}
+}
