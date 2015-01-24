60ec710908e94a065c063945f5d18dc036d7f819
Bradley Crusco <bcrusco627@gmail.com>
2015-01-24 13:14:38 -0500

    Added rough version of lives remaining and current level text to the UI.

---
+++ Assets/LevelUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelUI : MonoBehaviour {
+	Global globalObj;
+	Text levelText;
+	// Use this for initialization
+	void Start () {
+		GameObject g = GameObject.Find ("GlobalObject");
+		globalObj = g.GetComponent<Global>();
+		levelText = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		levelText.text = globalObj.level.ToString ();
+	}
+}
---
+++ Assets/LivesUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LivesUI : MonoBehaviour {
+	Global globalObj;
+	Text livesText;
+	// Use this for initialization
+	void Start () {
+		GameObject g = GameObject.Find ("GlobalObject");
+		globalObj = g.GetComponent<Global>();
+		livesText = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		livesText.text = globalObj.livesLeft.ToString ();
+	}
+}
